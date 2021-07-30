73652fceb2f2e44d1ed134f3282160bf312c46ef
mhuy10 <minhhuy101100@gmail.com>
2021-07-30 11:02:06 +0700

    Initial Setup

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const customerRouter=require('./routes/customer')
 const indexRouter=require('./routes/index')
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
-
+require('dotenv').config()
 const app=express()
 
 //connect database
@@ -15,18 +15,19 @@ app.use(express.urlencoded({extended: false}))
 
 const connectFunction = async()=>{
     try{
-        await mongoose.connect('mongodb://localhost/bai1')
+        // await mongoose.connect('mongodb://localhost/bai1')
+        await mongoose.connect(process.env.STR_CONNECT)
         console.log("Connected succesfully")
     }catch (e) {
-        console.log(e)
+        
         console.log("Connect failed")
     }
 }
 
 connectFunction()
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
+// app.set('view engine', 'ejs')
+// app.use(express.urlencoded({extended: false}))
 app.use('/customer/',customerRouter)
 app.use('/',indexRouter)
 
-app.listen(3000)
+app.listen(process.env.PORT||3000)
